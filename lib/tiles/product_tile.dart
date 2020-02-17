7372d067e3be85d104319bcf3e572224b3afbd21
Filipe Veber <FilipeVeber@users.noreply.github.com>
2020-02-17 13:18:35 -0300

    Grid e Tile da Grid de produtos

---
+++ lib/tiles/product_tile.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_loja_online/datas/product_data.dart';
+
+class ProductTile extends StatelessWidget {
+  final String _type;
+  final ProductData _productData;
+
+  ProductTile(this._type, this._productData);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      child: Card(
+        child: _type == "grid"
+            ? Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  AspectRatio(
+                    aspectRatio: 0.8,
+                    child: Image.network(
+                      _productData.images[0],
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      padding: EdgeInsets.all(8),
+                      child: Column(
+                        children: <Widget>[
+                          Text(
+                            _productData.title,
+                            style: TextStyle(fontWeight: FontWeight.w500),
+                          ),
+                          Text(
+                            "R\$${_productData.price.toStringAsFixed(2)}",
+                            style: TextStyle(
+                                color: Theme.of(context).primaryColor,
+                                fontSize: 17,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              )
+            : Row(),
+      ),
+    );
+  }
+}
