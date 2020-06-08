9a77ad7376db6f233ddda9769b253c9115aabdde
Filipe Veber <filipe.veber@supero.com.br>
2020-06-08 10:46:11 -0300

    Store tab

--- lib/tiles/store_tile.dart
+++ lib/tiles/store_tile.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class StoreTile extends StatelessWidget {
   final DocumentSnapshot store;
@@ -8,17 +9,54 @@ class StoreTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Card(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Text(store["title"]),
-            Text(store["address"]),
-          ],
-        ),
+    return Card(
+      margin: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          SizedBox(
+              height: 100,
+              child: Image.network(
+                store.data["image"],
+                fit: BoxFit.cover,
+              )),
+          Container(
+            padding: EdgeInsets.all(8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  store.data["title"],
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                ),
+                Text(store.data["address"]),
+              ],
+            ),
+          ),
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.map),
+                  color: Colors.blue,
+                  onPressed: () {
+                    launch(
+                        "https://www.google.com/maps/search/?api=1&query=${store.data["lat"]},${store.data["lng"]}");
+                  },
+                ),
+                IconButton(
+                  icon: Icon(Icons.call),
+                  color: Colors.blue,
+                  onPressed: () {
+                    launch("tel:${store.data["phone"]}");
+                  },
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
-    ;
   }
 }
